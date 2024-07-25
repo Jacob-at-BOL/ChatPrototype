273b1439345f9d2abe27cc02651f6e0e36435b18
Jake Simpson <102685368+Jacob-at-BOL@users.noreply.github.com>
2024-07-25 11:32:06 -0400

    Update ContentView.swift

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tues", isRainy: true, high: 60, low: 40)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return .blue
+        } else {
+            return .yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            
+            Image(systemName: iconName)
+                .foregroundColor(iconColor)
+                .font(.largeTitle)
+                .padding(5)
+            
+            Text("High: \(high)")
+                .fontWeight(.semibold)
+            
+            Text("Low: \(low)")
+                .fontWeight(.medium)
+                .foregroundColor(.secondary)
+        }
+        .padding()
+    }
 }
